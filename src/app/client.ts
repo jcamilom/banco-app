c9abaad48453188f65752ce26561b2c452dae61b
jcamilom <jcamilomunozz@gmail.com>
2018-06-03 17:51:24 -0500

    Adding Client class

---
+++ src/app/client.ts
@@ -0,0 +1,11 @@
+export class Client {
+
+  constructor(
+    public id: number,
+    public idType: string,
+    public name: string,
+    public lastname: string,
+    public birthdate: string
+  ) {  }
+
+}
