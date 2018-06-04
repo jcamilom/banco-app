53573634455ec4d26c5ab05226ca12b2bb81cb94
jcamilom <jcamilomunozz@gmail.com>
2018-06-04 17:08:51 -0500

    Clearing form

--- src/app/reg-form/reg-form.component.ts
+++ src/app/reg-form/reg-form.component.ts
@@ -20,19 +20,46 @@ export class RegFormComponent implements OnInit {
     this.getClients();
   }
 
-  getClients(): void {
+  private getClients(): void {
     this.clientService.getClients()
       .subscribe(clients => console.log(clients));
   }
 
-  addClient(): void {
+  private addClient(): void {
     this.clientService.addClient(this.client)
       .subscribe(client => console.log(client));
   }
 
   onSubmit() {
     this.submitted = true;
-    this.addClient();
+    // Add client if not exist
+    this.addClientIfNotExist();
+  }
+
+  /** This function get all the clients and add the new client
+  * only if it doesn't exist already.
+  * TODO: Modify the implementation once Firebase is changed to own API
+  * with SQL Server
+  * The id validation should be done when the input gets a new value
+  */
+  private addClientIfNotExist() {
+    this.clientService.getClients()
+      .subscribe(clients => {
+        let exist = false;
+        for(let key in clients) {
+          if(clients.hasOwnProperty(key)) {
+            if(clients[key].id == this.client.id) {
+              exist = true;
+              break;
+            }
+          }
+        }
+        if(exist == true) {
+          console.log("client already exist");
+        } else {
+          this.addClient();
+        }
+      });
   }
 
 }
