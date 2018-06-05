a318a90ea2e7db9596d2f35ba7647ec003c8b05a
jcamilom <jcamilomunozz@gmail.com>
2018-06-04 23:48:59 -0500

    Change LoanForm to ReactiveForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 // Services
@@ -26,6 +26,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule
   ],
--- src/app/components/loan-form/loan-form.component.ts
+++ src/app/components/loan-form/loan-form.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit }                  from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
-import { Client } from '../../client';
-
+import { Client} from '../../client';
 @Component({
   selector: 'app-loan-form',
   templateUrl: './loan-form.component.html',
@@ -9,11 +9,32 @@ import { Client } from '../../client';
 })
 export class LoanFormComponent implements OnInit {
 
-  client = new Client(0, '', '', '', '', '', 'Servientrega', 12345678, 2000000, '2015-01-15')
+  client = new Client(0, '', '', '', '', '', 'Servientrega', 12345678, 2000000, '2015-01-15');
 
-  constructor() { }
+  loanForm: FormGroup;
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.loanForm = new FormGroup({
+      'companyName': new FormControl(this.client.companyName, [
+        Validators.required,
+        /* Validators.minLength(4) */
+      ]),
+      'nit': new FormControl(this.client.nit, Validators.required),
+      'salary': new FormControl(this.client.salary, [
+        Validators.required
+      ]),
+      'entryDate': new FormControl(this.client.entryDate, [
+        Validators.required
+      ])
+    });
   }
+
+  get companyName() { return this.loanForm.get('companyName'); }
+
+  get nit() { return this.loanForm.get('nit'); }
+
+  get salary() { return this.loanForm.get('salary'); }
+
+  get entryDate() { return this.loanForm.get('entryDate'); }
 
 }
